7ad4f39ae1bbc7d98eb74459f1aa710b63685077
crayola-eater <a-sheikh@outlook.com>
2020-12-31 13:10:29 +0000

    Started HomePage, other pages too

--- fact-game/src/pages/Home/Home.js
+++ fact-game/src/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import styles from './Home.module.css'
+import Title from '../../fact-components/Title/Title'
+import Input from '../../fact-components/Input/Input'
 
 const pages = {
     CREATE: 'CREATE',
@@ -12,20 +14,18 @@ const pages = {
 export default function Home() {
     const [page, setPage] = useState('')
 
-    // Header
-
     if (!page) {
         return (
             <>
                 <header>X</header>
                 <main className={styles.pageContainer}>
-                    <h1>The Fact Game</h1>
+                    <Title text="The Fact Game" />
                     <div className={styles.buttonContainer}>
                         <button onClick={() => setPage(pages.CREATE)}>
                             Create New Game
                         </button>
                         <button onClick={() => setPage(pages.JOIN)}>
-                            Join Existing
+                            Join Existing Game
                         </button>
                     </div>
                 </main>
@@ -33,8 +33,39 @@ export default function Home() {
         )
     }
 
+    // CREATE
     if (page === pages.CREATE) {
+        return (
+            <>
+                <header>X</header>
+                <main className={styles.pageContainer}>
+                    <Title text="The Fact Game" />
+                    <Input placeholder="Enter number of rounds..." />
+                    <div className={styles.inputContainer}>
+                        <textarea placeholder="Enter a fact"></textarea>
+                        <textarea placeholder="Enter a lie"></textarea>
+                    </div>
+                    <button className={styles.createJoinButton}>CREATE</button>
+                </main>
+            </>
+        )
     }
+
+    // JOIN
+    return (
+        <>
+            <header>X</header>
+            <main className={styles.pageContainer}>
+                <Title text="The Fact Game" />
+                <Input placeholder="Enter the Game ID..." />
+                <div className={styles.inputContainer}>
+                    <textarea placeholder="Enter a fact"></textarea>
+                    <textarea placeholder="Enter a lie"></textarea>
+                </div>
+                <button className={styles.createJoinButton}>JOIN</button>
+            </main>
+        </>
+    )
 
     // <main> tag, stick everything else in this
     // flex, cent
