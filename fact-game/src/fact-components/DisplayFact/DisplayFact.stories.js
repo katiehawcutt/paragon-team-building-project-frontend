af5ea55133a095bde96e9cda46f74979a6dc5a64
= <=>
2021-01-03 11:54:59 +0000

    Whose Fact and Choose False Fact pages

--- fact-game/src/fact-components/DisplayFact/DisplayFact.stories.js
+++ fact-game/src/fact-components/DisplayFact/DisplayFact.stories.js
@@ -6,9 +6,12 @@ export default {
     component: DisplayFact,
 }
 
-const Template = (args) => <Title {...args} />
+const Template = (args) => <DisplayFact {...args} />
 
 export const DisplayFactStory = Template.bind({})
 DisplayFactStory.args = {
-    facts: ['I love cake'],
+    facts: [
+        `I hate cake very very much and can't even stand to be near it`,
+        'I once a skydive and my parachute failed but luckily a large bird saved me',
+    ],
 }
--- fact-game/src/pages/ChooseFalseFact/ChooseFalseFact.js
+++ fact-game/src/pages/ChooseFalseFact/ChooseFalseFact.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import styles from './ChooseFalseFact.module.css'
+import Header from '../../fact-components/Header/Header'
 import Round from '../../fact-components/Round/Round'
+import Title from '../../fact-components/Title/Title'
+import Countdown from '../../fact-components/Countdown/Countdown'
+import Fact from '../../fact-components/Fact/Fact'
 
-export default function ChooseFalseFact() {
+export default function ChooseFalseFact({
+    roundNumber,
+    displayName,
+    facts,
+    handleAnswer,
+    secondsLeft,
+    turnId,
+}) {
     return (
         <>
-            <header>X</header>
+            <Header />
             <main className={styles.pageContainer}>
-                <Round />
-                <Title text="" />
-                <p>10</p>
+                <Round roundNumber="1" />
+                <Title text="Which is Khang's truthy?" />
+                <Countdown secondsLeft="10" />
+                <div className={styles.factContainer}>
+                    <Fact
+                        factText={`I hate cake very very much and can't even stand to be near it`}
+                        // selected={selected}
+                        // handleClick={handleClick}
+                    />
+                    <Fact  factText={'I once a skydive and my parachute failed but luckily a large bird saved me'}/>
+                </div>
             </main>
         </>
     )
