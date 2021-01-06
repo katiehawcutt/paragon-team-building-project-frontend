d27b83cccb837f6914acf00eb193dd4afae69af2
= <=>
2021-01-06 12:02:21 +0000

    initial commit

--- platform/src/pages/Home/Home.js
+++ platform/src/pages/Home/Home.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import styles from './Home.module.css'
+import Background from '../../components/Background/Background'
+import Button from '../../components/Button/Button'
+import Title from '../../components/Title/Title'
 
 function Home() {
     return (
-        <div
-            style={{
+        <div className={styles.home_container}>
+            <Background />
+            <Button />
+            <Title />
+            style=
+            {{
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
                 height: '90vh',
             }}
-        >
             <h1>Home</h1>
         </div>
     )
