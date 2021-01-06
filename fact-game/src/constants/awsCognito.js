4f0cabb35ebae7afad5c401fef923e769de80de7
= <=>
2021-01-06 18:59:11 +0000

    putting callback urls back to env

--- fact-game/src/constants/awsCognito.js
+++ fact-game/src/constants/awsCognito.js
@@ -24,11 +24,10 @@ export const endpoints = Object.freeze({
  * These URLs need to match what we've configured in AWS Cognito.
  */
 export const callbackUrls = Object.freeze({
-    // AUTH_URL: process.env.REACT_APP_AUTH_URL ?? 'http://localhost:3000/login',
-
-    AUTH_URL: 'http://localhost:3000/login',
-    // LOGOUT_URL:
-    //     process.env.REACT_APP_LOGOUT_URL ??
-    //     'http://localhost:3000/after-logout',
-    LOGOUT_URL: 'http://localhost:3000/after-logout',
+    AUTH_URL: process.env.REACT_APP_AUTH_URL ?? 'http://localhost:3000/login',
+    // AUTH_URL: 'http://localhost:3000/login',
+    LOGOUT_URL:
+        process.env.REACT_APP_LOGOUT_URL ??
+        'http://localhost:3000/after-logout',
+    // LOGOUT_URL: 'http://localhost:3000/after-logout',
 })
