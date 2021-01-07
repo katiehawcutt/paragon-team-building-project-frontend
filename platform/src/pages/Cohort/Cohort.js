1281f0981285775f9544a671e394686eaa6fcb83
= <=>
2021-01-07 14:31:17 +0000

    cohortpage

--- platform/src/pages/Cohort/Cohort.js
+++ platform/src/pages/Cohort/Cohort.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import styles from './Cohort.module.css'
+import FlipCard from '../../components/FlipCard/FlipCard'
 
 function Cohort() {
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '90vh',
-            }}
-        >
-            <h1>Cohort</h1>
+        <div className={styles.pageContainer}>
+            <div className={styles.flipCardContainer}>
+                <FlipCard />
+                <FlipCard />
+                <FlipCard />
+                <FlipCard />
+                <FlipCard />
+                <FlipCard />
+                <FlipCard />
+                <FlipCard />
+            </div>
         </div>
     )
 }
