9c46f1a60d62c7ab6f923227ba00f60e0c7135d4
crayola-eater <a-sheikh@outlook.com>
2021-01-11 10:43:29 +0000

    Added gold, silver, bronze circles for top 3

--- fact-game/src/components/Leaderboard/Leaderboard.js
+++ fact-game/src/components/Leaderboard/Leaderboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Leaderboard.module.css'
+import cn from 'classnames'
 
 export default function LeaderBoard({ leaderboard }) {
     return (
@@ -14,16 +15,23 @@ export default function LeaderBoard({ leaderboard }) {
                 </thead>
                 <tbody>
                     {leaderboard.map((person, i) => {
+                        /**
+                         * Apply a specific class if position is top 3, otherwise default
+                         * to generic other class.
+                         */
+                        const colourClass =
+                            [
+                                styles.circleFirstPlace,
+                                styles.circleSecondPlace,
+                                styles.circleThirdPlace,
+                            ][person.position - 1] ?? styles.circleOtherPlace
+
+                        const className = cn(styles.circle, colourClass)
+
                         return (
                             <tr key={i}>
                                 <td className={styles.leaderboardOrder}>
-                                    <span
-                                        className={
-                                            person.position <= 3
-                                                ? styles.circleTopThree
-                                                : styles.circle
-                                        }
-                                    >
+                                    <span className={className}>
                                         {person.position}
                                     </span>
                                 </td>
