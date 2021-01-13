844627b81492d1915ee880293cb8268538996499
= <=>
2021-01-13 14:21:39 +0000

    profile upload image fixed

--- platform/src/components/ProfileSettings/ImageUpload.js
+++ platform/src/components/ProfileSettings/ImageUpload.js
@@ -49,39 +49,26 @@ const ImageUpload = () => {
                     border: 'none',
                 }}
             >
-                <div
+                <img
+                    src="./Images/kh.png"
+                    onerror={{ display: 'none' }}
+                    alt="user_profile"
+                    ref={uploadedImage}
                     style={{
-                        webkitBorderRadius: '50px',
-                        mozBorderRadius: '50px',
-                        borderRadius: '50px',
-                        border: 'none',
+                        maxWidth: '100%',
+                        maxHeight: '100%',
+                        position: 'absolute',
                         height: '60px',
                         width: '60px',
-                        backgroundImage: "url('./Images/kh.png')",
-                        backgroundSize: 'cover',
+                        borderRadius: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        border: 'none',
                         backgroundPosition: 'center',
                     }}
                     onClick={() => imageUploader.current.click()}
-                >
-                    <img
-                        onerror={{ display: 'none' }}
-                        alt="user_profile"
-                        ref={uploadedImage}
-                        style={{
-                            maxWidth: '100%',
-                            maxHeight: '100%',
-                            position: 'absolute',
-                            height: '60px',
-                            width: '60px',
-                            borderRadius: '100%',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            border: 'none',
-                            backgroundPosition: 'center',
-                        }}
-                    />
-                </div>
+                />
             </div>
             Click To Upload Image
         </div>
