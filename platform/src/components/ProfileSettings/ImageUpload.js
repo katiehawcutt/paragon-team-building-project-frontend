0f6bca32525c74a6c1e8197b0e65468696900b29
= <=>
2021-01-13 13:27:28 +0000

    profile settings errors sorted

--- platform/src/components/ProfileSettings/ImageUpload.js
+++ platform/src/components/ProfileSettings/ImageUpload.js
@@ -39,32 +39,48 @@ const ImageUpload = () => {
             />
             <div
                 style={{
-                    height: '60px',
-                    width: '60px',
-                    borderRadius: '100% !important',
-                    backgroundImage: "url('./Images/userIcon.png')",
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
+                    height: '65px',
+                    width: '65px',
+                    borderRadius: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundImage: 'linear-gradient(#4a90e0, #3c9d74)',
+                    border: 'none',
                 }}
-                onClick={() => imageUploader.current.click()}
             >
-                <img
-                    alt="user"
-                    ref={uploadedImage}
+                <div
                     style={{
-                        maxWidth: '100%',
-                        maxHeight: '100%',
-                        position: 'absolute',
+                        webkitBorderRadius: '50px',
+                        mozBorderRadius: '50px',
+                        borderRadius: '50px',
+                        border: 'none',
                         height: '60px',
                         width: '60px',
-                        borderRadius: '100%',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        border: 'none',
+                        backgroundImage: "url('./Images/kh.png')",
+                        backgroundSize: 'cover',
                         backgroundPosition: 'center',
                     }}
-                />
+                    onClick={() => imageUploader.current.click()}
+                >
+                    <img
+                        onerror={{ display: 'none' }}
+                        ref={uploadedImage}
+                        style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%',
+                            position: 'absolute',
+                            height: '60px',
+                            width: '60px',
+                            borderRadius: '100%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            border: 'none',
+                            backgroundPosition: 'center',
+                        }}
+                    />
+                </div>
             </div>
             Click To Upload Image
         </div>
